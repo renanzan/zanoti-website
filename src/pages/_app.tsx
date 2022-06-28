940404b79cd688fe7985f6ba185f21e8ed85944f
Renan Zanoti <renan.zanoti@aluno.ufop.edu.br>
2022-06-28 12:07:47 -0300

    fix: :bug: teste google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,27 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 import "../../public/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<Component {...pageProps} />
+		<>
+			<Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+			
+			<Script
+				strategy="lazyOnload"
+				dangerouslySetInnerHTML={{
+				__html: `
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+					page_path: window.location.pathname,
+					});
+				`,
+			}} />
+
+			<Component {...pageProps} />
+		</>
 	);
 }

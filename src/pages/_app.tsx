ab4aa77ad5f03bbe6b3fb423c234e8583dc75725
Renan Zanoti <renan.zanoti@aluno.ufop.edu.br>
2022-06-28 22:55:33 -0300

    fix: :bug: fix google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import { Fragment } from "react";
+import Script from "next/script";
 import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
@@ -18,6 +19,23 @@ export default function App({ Component, pageProps }: AppProps) {
 				pauseOnFocusLoss
 				draggable
 				pauseOnHover />
+
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=G-3WYVWTWSKD`} />
+
+			<Script
+				strategy="lazyOnload"
+				id="google-analytics">
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'G-3WYVWTWSKD', {
+					page_path: window.location.pathname,
+					});
+				`}
+			</Script>
 		</Fragment>
 	);
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,67 +1,51 @@
 import Document, { Head, Html, Main, NextScript, DocumentContext, DocumentInitialProps } from "next/document";
-import Script from "next/script";
 import { ServerStyleSheet } from "styled-components";
 
 class AppDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+	static async getInitialProps(
+		ctx: DocumentContext
+	): Promise<DocumentInitialProps> {
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App) => (props) =>
+						sheet.collectStyles(<App {...props} />),
+				});
 
-      const initialProps = await Document.getInitialProps(ctx);
+			const initialProps = await Document.getInitialProps(ctx);
 
-      return {
-        ...initialProps,
-        styles: [
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>,
-        ],
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
+			return {
+				...initialProps,
+				styles: [
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>,
+				],
+			};
+		} finally {
+			sheet.seal();
+		}
+	}
 
 	render(): JSX.Element {
 		return (
 			<Html>
 				<Head>
-          <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="128x128" href="/assets/favicon/favicon-128x128.png" />
-          <link rel="icon" type="image/png" sizes="152x152" href="/assets/favicon/favicon-152x152.png" />
-          <link rel="icon" type="image/png" sizes="167x167" href="/assets/favicon/favicon-167x167.png" />
-          <link rel="icon" type="image/png" sizes="180x180" href="/assets/favicon/favicon-180x180.png" />
-          <link rel="icon" type="image/png" sizes="192x192" href="/assets/favicon/favicon-192x192.png" />
-          <link rel="icon" type="image/png" sizes="196x196" href="/assets/favicon/favicon-196x196.png" />
-          <meta name="application-name" content="Renan Zanoti" />
-          <meta name="msapplication-TileColor" content="#25262A" />
-          <meta name="theme-color" content="#25262A" />
-          <meta charSet="utf-8" />
-
-          <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-			
-          <Script
-            strategy="lazyOnload"
-            dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-              });
-            `,
-          }} />
+					<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="128x128" href="/assets/favicon/favicon-128x128.png" />
+					<link rel="icon" type="image/png" sizes="152x152" href="/assets/favicon/favicon-152x152.png" />
+					<link rel="icon" type="image/png" sizes="167x167" href="/assets/favicon/favicon-167x167.png" />
+					<link rel="icon" type="image/png" sizes="180x180" href="/assets/favicon/favicon-180x180.png" />
+					<link rel="icon" type="image/png" sizes="192x192" href="/assets/favicon/favicon-192x192.png" />
+					<link rel="icon" type="image/png" sizes="196x196" href="/assets/favicon/favicon-196x196.png" />
+					<meta name="application-name" content="Renan Zanoti" />
+					<meta name="msapplication-TileColor" content="#25262A" />
+					<meta name="theme-color" content="#25262A" />
+					<meta charSet="utf-8" />
 				</Head>
 
 				<body>

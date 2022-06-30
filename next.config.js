5901086598bd3e633f8fe6510c2df01b26428ff6
Renan Zanoti <renan.zanoti@aluno.ufop.edu.br>
2022-06-30 19:28:06 -0300

    feat: :sparkles: projects redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,24 @@
 module.exports = {
 	images: {
 		domains: ["res.cloudinary.com"]
+	},
+	async redirects() {
+		return [
+			{
+				source: "/kderno",
+				destination: "/projects?current=kderno",
+				permanent: true
+			},
+			{
+				source: "/clipping",
+				destination: "/projects?current=clipping",
+				permanent: true
+			},
+			{
+				source: "/mentora",
+				destination: "/projects?current=mentora",
+				permanent: true
+			}
+		]
 	}
 }
